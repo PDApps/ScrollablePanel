6d4335645b5f21a5908ef5d52913b951fb26a453
Andrei Churila <AndrewChuriloMinsk@gmail.com>
2020-09-30 11:19:00 +0300

    Revert "Increase width of bold line"
    This reverts commit f5811975405bcda90c1dfad3b1e7aa05e82b24bc.

--- library/src/main/java/com/kelin/scrollablepanel/library/DottedLine.java
+++ library/src/main/java/com/kelin/scrollablepanel/library/DottedLine.java
@@ -101,8 +101,7 @@ public class DottedLine extends View {
             lineWidth = boldLineWidth;
         }
         paint.setStrokeWidth(lineWidth);
-        float lineXCoordinate = getWidth() / 2f;
-        canvas.drawLine(lineXCoordinate, 0, lineXCoordinate, getHeight(), paint);
+        canvas.drawLine(getWidth() / 2f, 0, getWidth() / 2f, getHeight(), paint);
     }
 
     public void setDotted(boolean dotted) {
